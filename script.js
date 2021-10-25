ac0f590cfcd28d7662ba7edbc53891e7734d0552
Breanne <BreMarotta@gmail.com>
2021-10-25 19:12:54 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,11 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "Knock, knock...knockeded knockeded";
+document.querySelector("body").appendChild(h3);
+
+const nikeo = document.createElement("Nikeo");
+nikeo.textContent = "Nikeo is, by far, the coolest of the cool. And he has major basketball skills!";
+document.querySelector("body").appendChild(nikeo);
